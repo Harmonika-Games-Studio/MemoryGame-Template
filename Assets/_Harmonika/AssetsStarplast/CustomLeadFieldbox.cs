179f7e81cbdc4a138650d343b14fcdb4111f2e5d
Christian Beninca <christianbeninca@outlook.com>
2025-05-29 11:45:01 -0300

    Validação de cpf

--- Assets/_Harmonika/AssetsStarplast/CustomLeadFieldbox.cs
+++ Assets/_Harmonika/AssetsStarplast/CustomLeadFieldbox.cs
@@ -1,10 +1,23 @@
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using Harmonika.Tools;
 using Harmonika.Tools.Keyboard;
+using Newtonsoft.Json.Linq;
 using TMPro;
 using UnityEngine;
+using System.IO;
+using System.Linq;
 
 public class CustomLeadFieldbox : LeadFieldBox
 {
+    string _permanentDataPath;
+
+
+    private void Awake()
+    {
+        _permanentDataPath = Application.persistentDataPath + "/permanentLocalData.json";
+    }
+
     public override void ApplyConfig(LeadDataConfig config)
     {
         _configuredByCode = true;
@@ -26,19 +39,34 @@ public class CustomLeadFieldbox : LeadFieldBox
                 {
                     _input_field.text = a.Format(config.inputDataConfig.parseableFields);
 
+
                     if (ID == LeadID.cpf)
                     {
+                        a = a.Format(ParseableFields.cpf);
+
                         if (!a.ValidateCPF())
                         {
                             _input_field.text = string.Empty;
-                            placeholderTMP.text = "Insira um CPF valido!";
-                            InvokeUtility.Invoke(2f, () => placeholderTMP.text = config.inputDataConfig.fieldPlaceholder);
+                            //placeholderTMP.text = "Insira um CPF valido!";
+                            //InvokeUtility.Invoke(2f, () => placeholderTMP.text = config.inputDataConfig.fieldPlaceholder);
+                            PopupManager.Instance.InvokeConfirmDialog("O CPF inserido é inválido! \n Por favor, tente novamente!", "OK", true, null, 0);
                             return;
                         }
 
 
+                        if (IsCpfRegistered(a))
+                        {
+                            _input_field.text = string.Empty;
+                            //placeholderTMP.text = $"Esse CPF já foi cadastrado.";
+                            //InvokeUtility.Invoke(2f, () => placeholderTMP.text = config.inputDataConfig.fieldPlaceholder);
+                            PopupManager.Instance.InvokeConfirmDialog("O CPF inserido já foi utilizado! \n Por favor, insira um outro CPF!", "OK", true, null, 0);
+                            return;
+                        }
                     }
+
                 });
+
+
                 _input_field.onValueChanged.AddListener((string a) => onValueChanged.Invoke(a));
                 break;
 
@@ -58,5 +86,40 @@ public class CustomLeadFieldbox : LeadFieldBox
                 _toggle.onValueChanged.AddListener((bool a) => onValueChanged.Invoke(a.ToString()));
                 break;
         }
+    }
+
+    private bool IsCpfRegistered(string cpf)
+    {
+        if (!File.Exists(_permanentDataPath))
+        {
+            Debug.LogError("Ainda não tem dados pra buscar.");
+            return false;
+        }
+
+        string jsonContent = File.ReadAllText(_permanentDataPath);
+
+        JObject jsonData;
+        try
+        {
+            jsonData = JObject.Parse(jsonContent);
+        }
+        catch
+        {
+            Debug.LogError("Erro ao processar o arquivo JSON. Verifique o formato.");
+            return false;
+        }
+
+        if (jsonData.ContainsKey("Data"))
+        {
+            var dataList = jsonData["Data"].ToObject<List<JObject>>();
+
+            return dataList.Any(record =>
+            {
+                var recordCpf = record["cpf"]?.ToString();
+                return recordCpf != null && recordCpf.Equals(cpf);
+            });
+        }
+
+        return false;
     }
 }
