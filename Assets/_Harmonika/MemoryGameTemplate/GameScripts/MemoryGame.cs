aedf3e260c6cac06f17872fc083cea601d694e80
Pedro Fornazari <pedrofornazari02@gmail.com>
2025-06-04 18:22:00 -0300

    First art implementation VW

--- Assets/_Harmonika/MemoryGameTemplate/GameScripts/MemoryGame.cs
+++ Assets/_Harmonika/MemoryGameTemplate/GameScripts/MemoryGame.cs
@@ -277,7 +277,7 @@ public class MemoryGame : MonoBehaviour
         // Configurar o GridLayoutGroup com a quantidade de colunas
         gridLayoutGroup.constraint = GridLayoutGroup.Constraint.FixedColumnCount;
         gridLayoutGroup.constraintCount = numberOfColumns;
-        gridLayoutGroup.cellSize = new Vector2(cellWidth, cellHeight);
+        //gridLayoutGroup.cellSize = new Vector2(cellWidth, cellHeight);
 
     }
 
